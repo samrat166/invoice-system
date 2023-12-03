249d0120bcf6d7211ac2698e5403881c5ccee56f
tarmas91011@16 <samratojha16@gmail.com>
2023-12-03 11:56:13 +0545

    -price list page

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 function toggleLanguageOptions() {
-    var options = document.getElementById("languageOptions");
-    options.style.display = options.style.display === "block" ? "none" : "block";
+  var options = document.getElementById("languageOptions");
+  options.style.display = options.style.display === "block" ? "none" : "block";
 }
 
 function selectLanguage(langCode, flagSrc, langName) {
-    // Set the selected language and update the display
-    var selectedLanguage = document.getElementById("selectedLanguage");
-    var flagImage = selectedLanguage.querySelector(".flag");
-    var languageName = selectedLanguage.querySelector("span");
+  var selectedLanguage = document.getElementById("selectedLanguage");
+  var flagImage = selectedLanguage.querySelector(".flag");
+  var languageName = selectedLanguage.querySelector("span");
 
-    flagImage.src = flagSrc;
-    flagImage.alt = langName + " Flag";
-    languageName.textContent = langName;
+  flagImage.src = flagSrc;
+  flagImage.alt = langName + " Flag";
+  languageName.textContent = langName;
 
-    console.log("Selected language: " + langCode);
-    toggleLanguageOptions(); // Close the options after selection
+  console.log("Selected language: " + langCode);
+  toggleLanguageOptions();
 }
 
 function onHamburgerClick() {
-
+  console.log("Clicked hamburger");
 }
+
+
